c23207d25d09fae43b811030813d33baaee526f2
Vladimir Elistratov <vvoovv.projects@gmail.com>
2012-06-30 14:21:56 +0400

    Starting InfoWindow Control

---
+++ InfoWindow.js
@@ -0,0 +1,24 @@
+define([
+	"dojo/_base/declare"
+], function(declare) {
+
+return declare(null, {
+	
+	init: function() {
+		this.infoWindow = new google.maps.InfoWindow();
+	},
+
+	process: function(event){
+		var feature = event.feature,
+			cs = feature.state.cs,
+			iw = this.infoWindow,
+			content = cs.info ? cs.info(feature) : this.content(feature)
+		;
+		iw.setPosition(event.event.latLng);
+		iw.setContent(content);
+		iw.open(this.map.engine.gmap);
+	}
+
+});
+
+});
