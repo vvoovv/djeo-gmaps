aaaee09545da9596b799aeb27dde6163e49223bd
Adrian Rakovsky <adrian.rakovsky@gratex.com>
2015-02-10 10:57:45 +0100

    CoordArray is now correctly not tagged as AMD module

--- djeo-gmaps.profile.js
+++ djeo-gmaps.profile.js
@@ -12,7 +12,7 @@ var profile = {
 			return copyOnly(filename, mid);
 		},
 		amd: function(filename, mid) {
-			return !copyOnly(mid) && /\.js$/.test(filename);
+			return "djeo-gmaps/CoordArray" != mid && !copyOnly(mid) && /\.js$/.test(filename);
 		},
 		miniExclude: function(filename, mid) {
 			return miniExclude(filename, mid);
